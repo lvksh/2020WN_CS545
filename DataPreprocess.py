911256a03df62499b0799778e0d2a10489b398ee
lvksh <lvksh@umich.edu>
2020-11-09 20:31:30 +0800

    Add some outline python code

--- DataPreprocess.py
+++ DataPreprocess.py
@@ -1,8 +1,14 @@
 # This script is for data preprocessing
-def assignNews(newsPath, mappingPath, outputPath):
+def assignNews(newsPath, mappingPath, outputPath, dailyPath):
     # TODO: assign news from newsPath to certain stock and output to the outputPath
-    # TODO: In the output path, each stock have one folder with all the related news
+    # outputPath
+    #   - stock1
+    #     - 20200101
+    #        - news1
+    #        - news2
+    #     - 20200102
     pass
+
 
 def assessNews(newsPath):
     # TODO: Calculate news count for each stock files
@@ -10,11 +16,24 @@ def assessNews(newsPath):
     # TODO: Find a way to assess the content of the news
     pass
 
-def dataPreprocess(dataPath,    # input path
-                   mappingPath, # stockid to name mapping path
-                   outputPath   # output path
-                  ):
-    # the input path has the sturcture like this
+def splitWords(newsPath):
+    # TODO: Use jieba to split words and remove stopwords iteratively for each news file
+    pass
+
+def getLabel(dailyPath):
+    # TODO: Perform a diff operation on the daily close price of all stocks and output label file
+    pass
+
+def buildData(dataPath):
+    # TODO: Find a way to organize the data for the convinience in future data loader
+    pass
+
+def dataPreprocess(dataPath,  # input path
+                   mappingPath,  # stockid to name mapping path
+                   outputPath,  # output path
+                   dailyPath    # daily info path
+                   ):
+    # the input path has the structure like this
     # dataPath
     #   - sinaNews
     #      - sina-yyyy-mm-dd.csv
@@ -35,9 +54,21 @@ def dataPreprocess(dataPath,    # input path
 
     # Assign news to different stocks
     for path in ['sinaNews', 'wsNews', 'thsNews', 'ycjNews', 'eastmoneyNews']:
-        assignNews(newsPath = dataPath + path,
-                   mappingPath = mappingPath,
-                   outputPath = outputPath)
+        assignNews(newsPath=dataPath + path,
+                   mappingPath=mappingPath,
+                   outputPath=outputPath,
+                   dailyPath=dailyPath)
 
-    # Assess the news
-    assessNews(newsPath = outputPath)
+    # In output path, stockid <== date <== news
+
+    # Assess the news and drop useless stocks
+    assessNews(newsPath=outputPath)
+
+    # Split words, remove stop words
+    splitWords(newsPath=outputPath)
+
+    # Get label data
+    getLabel(dailyPath=dailyPath)
+
+    # Organize the data with the label
+    buildData(dataPath=outputPath)
